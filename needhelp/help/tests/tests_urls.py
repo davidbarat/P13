b916aed1f625a2fd95e71e4d14eb4c7485798ef5
davidbarat <dav.barat@gmail.com>
2021-05-02 23:52:54 +0200

    test: first test

---
+++ needhelp/help/tests/tests_urls.py
@@ -0,0 +1,15 @@
+from django.test import TestCase, Client, RequestFactory
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+
+class UrlsTest(TestCase):
+
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(
+            username='david', email='david@gmail.com', password='csecret')
+
+    def test_index_page(self):
+        response = self.client.get(reverse("index"))
+        self.assertEqual(response.status_code, 200)
