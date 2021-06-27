2a647b889e2f24923e9455b71aa53c9c144a36b1
David <33658472+davidbarat@users.noreply.github.com>
2021-06-28 00:06:55 +0200

    feat: add send message to api

--- raspberry/socket_ir.py
+++ raspberry/socket_ir.py
@@ -1,7 +1,28 @@
 import socket
+import requests
+import json
+import time
+import os
+from datetime import datetime
+from gpiozero import LED
+from signal import pause
 
 SOCKPATH = "/var/run/lirc/lircd"
 sock = None
+led = LED(4)
+group_name = "Barat"
+now = datetime.now()
+dt_formated = now.strftime("%Y-%m-%dT%H:%M:%S")
+status = "open"
+token = os.getenv("TOKEN")
+url = 'http://167.71.57.121/event/list/'
+data = {
+    "date_event": dt_formated,
+    "group_name": group_name,
+    "status": status}
+headers = {
+    'Content-type': 'application/json',
+    'Authorization': 'Token d0c55c311586b688894de0d39e0a011f47cedee3'}
 
 # Establish a socket connection to the lirc daemon
 def init_irw():
@@ -34,6 +55,20 @@ if __name__ == '__main__':
        	    # Only print the name when the key is pressed (and not released)
        	    if (dir == '00' and key == 'BUTTON_1'):
        	        print ("BUTTON 1 PRESSED!")
+                r = requests.post(url, headers=headers, data=json.dumps(data)) 
+                # led.blink()
+                led.on()
+                time.sleep(1)
+                led.off()
+                time.sleep(1)
+                led.on()
+                time.sleep(1)
+                led.off()
+                time.sleep(1)
+                led.on()
+                time.sleep(1)
+                led.off()
+
     except KeyboardInterrupt:
     	print ("\nShutting down...")
     	# Close the socket (if it exists)
